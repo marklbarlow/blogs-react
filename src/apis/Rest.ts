617f4b5a47d1b023faddd8fb5071ccd7bc340872
marklbarlow <23555996+marklbarlow@users.noreply.github.com>
2023-02-24 20:31:10 +0000

    Simplify axios usage

--- src/apis/Rest.ts
+++ src/apis/Rest.ts
@@ -1,12 +1,10 @@
 import { api } from './configs/axiosConfigs';
 
 export const Rest = {
-  delete: async <T>(url: string): Promise<T> =>
-    (await api.request({ method: 'DELETE', url })).data,
-  get: async <T>(url: string): Promise<T> =>
-    (await api.request({ method: 'GET', url })).data,
+  delete: async <T>(url: string): Promise<T> => (await api.delete(url)).data,
+  get: async <T>(url: string): Promise<T> => (await api.get(url)).data,
   post: async <T>(url: string, data: unknown): Promise<T> =>
-    (await api.request({ data, method: 'POST', url })).data,
+    (await api.post(url, data)).data,
   put: async <T>(url: string, data: unknown): Promise<T> =>
-    (await api.request({ data, method: 'PUT', url })).data,
+    (await api.put(url, data)).data,
 };
