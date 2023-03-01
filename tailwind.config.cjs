32b372c764447dd575af6ab0777378abc0fb2d8b
Mark Barlow <23555996+marklbarlow@users.noreply.github.com>
2023-03-01 15:42:17 +0000

    Update config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+  plugins: [require('@tailwindcss/typography')],
+};
