d5a2b3197a22c0f93d10db7e89bdfed52334d11b
marklbarlow <23555996+marklbarlow@users.noreply.github.com>
2023-03-21 19:47:45 +0000

    Update styles

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -29,7 +29,7 @@ export const Card = ({
       ></div>
       <div>
         <button
-          className="bg-transparent hover:bg-blue-700 text-blue-500 font-semibold hover:text-white py-2 px-4 border border-blue hover:border-transparent rounded"
+          className="btn btn-outline"
           type="button"
           onClick={() => onReadClicked(preview.id)}
         >
